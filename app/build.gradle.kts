73e4fd6248f1111e9242703c3d5c2eb4067e83b9
Elietio <xzc549212830@gmail.com>
2026-05-17 22:40:08 +0800

    chore: bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "xyz.elietio.routineplus.isworkday"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     applicationVariants.all {
