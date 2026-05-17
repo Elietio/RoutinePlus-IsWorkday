5d23988dc13770f189093a5792ce28c94cb23f69
Elietio <xzc549212830@gmail.com>
2026-05-17 23:36:11 +0800

    style: crop app icon to circle and remove white background; bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "xyz.elietio.routineplus.isworkday"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.2"
+        versionCode = 5
+        versionName = "1.1.3"
     }
 
     applicationVariants.all {
