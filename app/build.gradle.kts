fd14c129776543159373a2c37c0e930f373c3c9d
Elietio <xzc549212830@gmail.com>
2026-05-22 21:05:53 +0800

    bump version to v1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "xyz.elietio.routineplus.isworkday"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.0"
+        versionCode = 11
+        versionName = "1.3.1"
     }
 
     applicationVariants.all {
